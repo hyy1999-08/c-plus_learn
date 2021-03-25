b29f33bc23d94f42e51aef9d9ff7057aa5ec67ef
hyy1999-08 <54015209+hyy1999-08@users.noreply.github.com>
2021-03-25 09:15:18 +0800

    Add files via upload

---
+++ section2_task5/section2_task5/mytext.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+template<typename elemtype>elemtype max1(elemtype t1, elemtype t2);
+
+
+template<typename vtype>vtype max(vector<vtype> myseq);
+
+
+template<typename atype>atype max(atype myseq[], const int size);
+
+
